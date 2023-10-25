1671a9c5f6c8921a3d389e58427cf8f171beefcb
Guilherme <guilherme.melo4631@gmail.com>
2023-10-24 22:56:07 -0300

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+const limit = 10
+const offset = 0
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
+
+fetch(url)
+.then((response) => response.json)
+.then((jsonBody) => console.log(jsonBody))
+.catch((error) => console.error(error))
